3f3df6a04ae0620115322b8e3b37a1a8ba6dfee9
Les Aubrey <les@wharf.digital>
2024-06-28 12:59:39 +0800

    Adds initial post schema

--- apps/wharf-digital-cms/schemas/index.ts
+++ apps/wharf-digital-cms/schemas/index.ts
@@ -1,9 +1,11 @@
 // Documents
 import settings from './documents/settings';
 import page from './documents/page';
+import post from './documents/post';
 
 export const schemaTypes = [
   // Documents
   settings,
   page,
+  post,
 ];
