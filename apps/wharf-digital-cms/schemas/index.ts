6846f3363ecdf6cdddaef6b2347b779510897be5
Les Aubrey <les@wharf.digital>
2024-06-28 13:45:13 +0800

    Adds initial project schema

--- apps/wharf-digital-cms/schemas/index.ts
+++ apps/wharf-digital-cms/schemas/index.ts
@@ -2,10 +2,12 @@
 import settings from './documents/settings';
 import page from './documents/page';
 import post from './documents/post';
+import project from './documents/project';
 
 export const schemaTypes = [
   // Documents
   settings,
   page,
   post,
+  project,
 ];
