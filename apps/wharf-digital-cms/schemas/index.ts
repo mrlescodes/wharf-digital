86170fa5c383fe28fd45a78277a16e4ab3ed5e63
Les Aubrey <les@wharf.digital>
2024-07-02 09:15:30 +0800

    Convert settings to a singleton

--- apps/wharf-digital-cms/schemas/index.ts
+++ apps/wharf-digital-cms/schemas/index.ts
@@ -1,12 +1,16 @@
+// Singletons
+import settings from './singletons/settings';
+
 // Documents
-import settings from './documents/settings';
 import page from './documents/page';
 import post from './documents/post';
 import project from './documents/project';
 
 export const schemaTypes = [
-  // Documents
+  // Singletons
   settings,
+
+  // Documents
   page,
   post,
   project,
