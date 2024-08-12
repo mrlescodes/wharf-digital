98c3b08382a06c9553138e071550c5952c4e2ecc
Les Aubrey <les@wharf.digital>
2024-08-12 11:28:40 +0800

    Adds team member schema

--- apps/wharf-digital-cms/schemas/index.ts
+++ apps/wharf-digital-cms/schemas/index.ts
@@ -9,6 +9,7 @@ import navigation from './documents/navigation';
 import page from './documents/page';
 import post from './documents/post';
 import project from './documents/project';
+import teamMember from './documents/team-member';
 
 // Modules
 import hero from './modules/hero';
@@ -28,6 +29,7 @@ export const schemaTypes = [
   page,
   post,
   project,
+  teamMember,
 
   // Modules
   hero,
